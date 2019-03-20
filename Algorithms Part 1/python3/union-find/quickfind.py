7d276d3bbaa806977a41e73546a55984fabfa09b
Ashish Ganta <ashishganta@gmail.com>
2019-03-20 15:42:09 -0700

    union find

--- Algorithms Part 1/python3/union-find/quickfind.py
+++ Algorithms Part 1/python3/union-find/quickfind.py
@@ -23,3 +23,22 @@ class quickfind(object):
 			for i in range(0,self.N):
 				if self.id[i] == b:
 					self.id[i] = a
+
+if __name__ == '__main__':
+	newQF = quickfind(10)
+	print(newQF.id)
+	newQF.union(4,3)
+	print(newQF.id)
+	newQF.union(3,8)
+	print(newQF.find(5,6))
+	newQF.union(6,5)
+	print(newQF.id)
+	newQF.union(9,4)
+	print(newQF.find(1,9))
+	newQF.union(8,9)
+	print(newQF.id)
+	newQF.union(8,9)
+	newQF.union(2,1)
+	newQF.union(7,6)
+	newQF.union(5,4)
+	print(newQF.id)
