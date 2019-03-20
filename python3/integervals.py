e51fdbc17ed73ab2db2af4b00a28f6a439d112b9
Ashish Ganta <ashishganta@gmail.com>
2019-03-20 12:15:16 -0700

    Initial Commit

---
+++ python3/integervals.py
@@ -0,0 +1,13 @@
+print("Equation: 8x + 6y = 2500")
+for x in range(30,90):
+	y = 2500 - 8*x
+	y = y/6
+	if (y.is_integer()):
+		print(x,y)
+
+print("Equation: 9x + 6y = 2500")
+for x in range(30,100,5):
+	y = 2500 - 9*x
+	y = y/6
+	print(x,y)
+
